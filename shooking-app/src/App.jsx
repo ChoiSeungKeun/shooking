4bbd6ddeab027f4e5cf0d9bf880059c00becde72
choiseungkeun <tmdrms0705@gmail.com>
2025-07-21 18:49:42 +0900

    feat: 기본 페이지 생성 및 라우터 추가

---
+++ shooking-app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Notfound from "./pages/Notfound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
+}
+
+export default App;
