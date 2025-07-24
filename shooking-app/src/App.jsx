b9904939ac1c0ec307faab3d13de61b75d874625
choiseungkeun <tmdrms0705@gmail.com>
2025-07-25 05:38:21 +0900

    fix: Github Pages에서 SPA 라우팅을 위한 코드 변경

--- shooking-app/src/App.jsx
+++ shooking-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
-        <Route path="*" element={<Notfound />} />
+        <Route path="/*" element={<Notfound />} />
       </Routes>
     </CartProvider>
   );
